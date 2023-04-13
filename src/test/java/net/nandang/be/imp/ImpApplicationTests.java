9cd36acaea89bb4c23faf70db7ba81b23fce42a0
netng <net.nandang@gmail.com>
2023-04-13 08:45:28 +0700

    first commit

---
+++ src/test/java/net/nandang/be/imp/ImpApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nandang.be.imp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
